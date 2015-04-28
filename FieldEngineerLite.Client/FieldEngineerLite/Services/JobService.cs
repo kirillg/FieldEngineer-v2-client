c6bb51819d2eb9b9f16db16ee942e4c5263acfc1
Donna Malayeri <donnam@microsoft.com>
2015-04-27 19:26:50 -0700

    Add an actual mock class. Still needs to be tested

--- FieldEngineerLite.Client/FieldEngineerLite/Services/JobService.cs
+++ FieldEngineerLite.Client/FieldEngineerLite/Services/JobService.cs
@@ -14,23 +14,35 @@ using System.Net.Http;
 using Xamarin.Forms;
 using System.Reflection;
 
-#if false
 namespace FieldEngineerLite
 {
 
     public class JobService
     {
+        #region Member variables
+        
+        public bool LoginInProgress = false;
+        public bool Online = false;        
+        
+        #endregion
+        
+        // 1. add client initializer
         public MobileServiceClient MobileService =
             new MobileServiceClient(
-            "https://fieldengineerlite-code.azurewebsites.net",
-            "https://fieldengineerlite-rg3de4f453d3934e29b477bd2b4ff43a83.azurewebsites.net",
-            "cPeHjNEDGgXLTnMWvJogCMUfyaQVlZ83"
-        );
+                "https://fieldengineerlite-code.azurewebsites.net",
+                "https://fieldengineerlite-rg3de4f453d3934e29b477bd2b4ff43a83.azurewebsites.net",
+                "cPeHjNEDGgXLTnMWvJogCMUfyaQVlZ83"
+            );
 
+        // 2. add sync table
+        private IMobileServiceSyncTable<Job> jobTable;
+          
+        
         public async Task InitializeAsync()
         {
-            //2. replace with init
-            var store = new MobileServiceSQLiteStore("localdb-fabrikam2.db");
+            // 3. initialize local store
+
+            var store = new MobileServiceSQLiteStore("local-db-fabrikamA");
             store.DefineTable<Job>();
 
             await MobileService.SyncContext.InitializeAsync(store);
@@ -40,28 +52,31 @@ namespace FieldEngineerLite
 
         public async Task<IEnumerable<Job>> ReadJobs(string search)
         {
-            //3. replace with read
+            // 4. read from local db
+            
             var query = jobTable.CreateQuery();
             if (string.IsNullOrEmpty(search) == false)
             {
                 query = query.Where(job => (job.Title == search));
             }
+            
             return await query.ToEnumerableAsync();
         }
 
         public async Task UpdateJobAsync(Job job)
         {
             job.Status = Job.CompleteStatus;
-            //4. add update
+            
+            // 5. update local db
             await jobTable.UpdateAsync(job);
         }
 
         public async Task SyncAsync()
         {
-            //6. add auth
+            // 7. add auth
 
 
-            //5. add sync
+            // 6. add sync
             try
             {
                 await this.MobileService.SyncContext.PushAsync();
@@ -71,17 +86,8 @@ namespace FieldEngineerLite
 
                 await jobTable.PullAsync(null, query);
             }
-            catch (Exception)
-            { 
-            }
-        }
-
-        private IMobileServiceSyncTable<Job> jobTable;
-
-        public async Task ClearAllJobs()
-        {
-            jobs = new List<Job>();
-            await Task.FromResult(0);
+            
+            catch (Exception) {  }
         }
 
         public async Task CompleteJobAsync(Job job)
@@ -91,53 +97,5 @@ namespace FieldEngineerLite
             if (Online)
                 await this.SyncAsync();
         }
-
-        private List<Job> GetDummyData()
-        {
-            return new List<Job> {
-                new Job 
-                { 
-                    AgentId = "agent1",
-                    CustomerName = "Lorem Ipsum",
-
-                    StartTime = "08:30",
-                    EndTime =  "09:30",
-                    Id = "1",
-                    JobNumber = "1",
-                    Status = Job.InProgressStatus,
-                    Title = "Dolor sit amet"
-                },
-                new Job 
-                { 
-                    AgentId = "agent1", 
-                    CustomerName = "Consectetur Adipiscing",
-                    StartTime = "10:30",
-                    EndTime = "11:30",
-                    Id = "2",
-                    JobNumber = "2",
-                    Status = Job.PendingStatus,
-                    Title = "Incididunt ut labore et dolore"
-                },
-                new Job 
-                { 
-                    AgentId = "agent1", 
-                    CustomerName = "Magna Aliqua",
-
-                    StartTime = "11:30",
-                    EndTime = "12:30",
-                    Id = "3",
-                    JobNumber = "3",
-                    Status = Job.CompleteStatus,
-                    Title = "Ut enim ad minim veniam"
-                }
-            };
-        }
-
-        private List<Job> jobs;
-        public bool LoginInProgress = false;
-        public bool Online = false;
-
     }
 }
-
-#endif
